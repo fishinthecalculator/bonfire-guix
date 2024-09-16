2cc6f76adafb6333f0ec3e5fe4835fa0f0d9a0ff
Giacomo Leidi <goodoldpaul@autistici.org>
2024-09-17 00:42:20 +0200

    bonfire: Add elixir-stream-data.
    * modules/bonfire/packages/elixir-xyz.scm (elixir-stream-data): New variable.

--- modules/bonfire/packages/elixir-xyz.scm
+++ modules/bonfire/packages/elixir-xyz.scm
@@ -24,3 +24,22 @@
 providing @code{Access} behaviour for custom structs.")
     (home-page "https://hexdocs.pm/accessible/")
     (license license:expat)))
+
+(define-public elixir-stream-data
+  (package
+    (name "elixir-stream-data")
+    (version "1.1.1")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (hexpm-uri "stream_data" version))
+       (sha256
+        (base32 "134c7dyvprvnfm7fz6dvhk1mifrd0iq2pwjkzmiq8wq6pm3cvl25"))))
+    (build-system mix-build-system)
+    (native-inputs
+     (list elixir-excoveralls))
+    (synopsis "Data generation and property-based testing for Elixir")
+    (description "@code{StreamData} is an Elixir library for data generation and
+property-based testing.")
+    (home-page "https://hexdocs.pm/stream_data/")
+    (license license:asl2.0)))
