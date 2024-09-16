61be4a8af739ed9e9dcb78ea3f33dadb92b39cf3
Giacomo Leidi <goodoldpaul@autistici.org>
2024-09-17 00:49:53 +0200

    bonfire: Add elixir-jason.
    * modules/bonfire/packages/elixir-xyz.scm (elixir-jason): New variable.

--- modules/bonfire/packages/elixir-xyz.scm
+++ modules/bonfire/packages/elixir-xyz.scm
@@ -46,6 +46,20 @@ places can be represented exactly.")
     (home-page "https://hexdocs.pm/decimal/")
     (license license:asl2.0)))
 
+(define-public elixir-jason
+  (package
+    (inherit elixir-jason)
+    (version "1.4.4")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (hexpm-uri "jason" version))
+       (sha256
+        (base32 "0fqbr5n5z3cya8a7i9i1kb38wsi314sccnymjjgmk57hj6mhrsy5"))))
+    (native-inputs
+     (list elixir-stream-data))
+    (inputs (list elixir-decimal))))
+
 (define-public elixir-stream-data
   (package
     (name "elixir-stream-data")
