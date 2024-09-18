2855ce86d623243b319958d9dc59f2bd326d0664
Giacomo Leidi <goodoldpaul@autistici.org>
2024-09-18 02:16:25 +0200

    bonfire: Add elixir-earmark-ast-dsl.
    * modules/bonfire/packages/elixir-markup.scm (elixir-earmark-ast-dsl): New variable.

---
+++ modules/bonfire/packages/elixir-markup.scm
@@ -0,0 +1,31 @@
+;;; SPDX-License-Identifier: GPL-3.0-or-later
+;;; Copyright © 2024 Giacomo Leidi <goodoldpaul@autistici.org>
+
+(define-module (bonfire packages elixir-markup)
+  #:use-module (gnu packages elixir-xyz)
+  #:use-module (guix download)
+  #:use-module (guix gexp)
+  #:use-module ((guix licenses)
+                #:prefix license:)
+  #:use-module (guix packages)
+  #:use-module (bonfire guix build-system mix))
+
+(define-public elixir-earmark-ast-dsl
+  (package
+    (name "elixir-earmark-ast-dsl")
+    (version "0.3.7")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (hexpm-uri "earmark_ast_dsl" version))
+       (sha256
+        (base32 "1c90204jcz1pbxxdxmvwr32y663jwvpn05izqp7zmqlp8yknzlhj"))))
+    (build-system mix-build-system)
+    (synopsis
+     "Toolset to generate EarmarkParser AST Nodes")
+    (description
+     "@code{EarmarkAstDsl} is a toolset to generate @code{EarmarkParser}
+conformant AST Nodes.  Its main purpose is to remove boilerplate code from
+Earmark and @code{EarmarkParser} tests.")
+    (home-page "https://hexdocs.pm/earmark_ast_dsl/")
+    (license license:asl2.0)))
