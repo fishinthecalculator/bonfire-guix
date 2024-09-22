2fd4d2e886acdc9ca4df2d0411f1fd3cae28fe8e
Giacomo Leidi <goodoldpaul@autistici.org>
2024-09-22 23:33:37 +0200

    bonfire: packages: Sort alphabetically.

--- modules/bonfire/packages/elixir-i18n.scm
+++ modules/bonfire/packages/elixir-i18n.scm
@@ -11,26 +11,6 @@
   #:use-module (bonfire guix build-system mix)
   #:use-module (bonfire packages elixir-xyz))
 
-(define-public elixir-digital-token
-  (package
-    (name "elixir-digital-token")
-    (version "1.0.0")
-    (source
-     (origin
-       (method url-fetch)
-       (uri (hexpm-uri "digital_token" version))
-       (sha256
-        (base32 "1rp9r5jb0db52f969jmgv5sp8k0vd98dnqwrgca0fyzsqalgbmlf"))))
-    (build-system mix-build-system)
-    (propagated-inputs (list elixir-cldr-utils elixir-jason))
-    (synopsis
-     "Elixir integration for ISO 24165 Digital Tokens.")
-    (description
-     "Elixir integration for ISO 24165 Digital Tokens (crypto currencies) through the
-DTIF registry data.")
-    (home-page "https://hexdocs.pm/digital_token/")
-    (license license:asl2.0)))
-
 (define-public elixir-cldr-utils
   (package
     (name "elixir-cldr-utils")
@@ -49,6 +29,26 @@ DTIF registry data.")
      "@code{Map, @code{Calendar}, @code{Digits}, @code{Decimal}, @code{HTTP},
 @code{Macro}, @code{Math}, and @code{String} helpers for @code{ex_cldr}.")
     (home-page "https://hexdocs.pm/cldr_utils/")
+    (license license:asl2.0)))
+
+(define-public elixir-digital-token
+  (package
+    (name "elixir-digital-token")
+    (version "1.0.0")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (hexpm-uri "digital_token" version))
+       (sha256
+        (base32 "1rp9r5jb0db52f969jmgv5sp8k0vd98dnqwrgca0fyzsqalgbmlf"))))
+    (build-system mix-build-system)
+    (propagated-inputs (list elixir-cldr-utils elixir-jason))
+    (synopsis
+     "Elixir integration for ISO 24165 Digital Tokens.")
+    (description
+     "Elixir integration for ISO 24165 Digital Tokens (crypto currencies) through the
+DTIF registry data.")
+    (home-page "https://hexdocs.pm/digital_token/")
     (license license:asl2.0)))
 
 (define-public elixir-ex-cldr
